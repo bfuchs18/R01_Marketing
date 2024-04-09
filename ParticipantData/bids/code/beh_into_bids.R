db52238a176bea68f5a9658fa01ee2eb2c7c861b
bfuchs18 <bari.fuchs@gmail.com>
2024-04-09 13:14:42 -0400

    organize on one drive and use proc_task()

--- ParticipantData/bids/code/beh_into_bids.R
+++ ParticipantData/bids/code/beh_into_bids.R
@@ -1,25 +1,26 @@
-# Process and organize survey (Redcap) and task data into bids
+# Process and organize data into bids
 
-library(dataREACHr)
+#### setup ####
+
+# load dataREACHr
+load_all("/Users/baf44/projects/dataREACHr")
+
+# set path to base directory (contains bids/ and untouchedRaw/)
+# base_dir <- "/Users/baf44/projects/Keller_Marketing/ParticipantData/"
+base_dir <- "/Users/baf44/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniversity/b-childfoodlab_Shared/Active_Studies/MarketingResilienceRO1_8242020/ParticipantData/"
 
 #### process redcap data ####
 
-# assign paths to data downloaded from redcap
-visit_data_path = "/Users/baf44/projects/Keller_Marketing/ParticipantData/bids/sourcedata/phenotype/FoodMarketingResilie_DATA_2024-03-22_1446.csv"
-data_de_path = "/Users/baf44/projects/Keller_Marketing/ParticipantData/bids/sourcedata/phenotype/REACHDataDoubleEntry_DATA_2024-03-12_1045.csv"
+# set redcap file names
+visit_file_name =  "FoodMarketingResilie_DATA_2024-04-08_1303.csv"
+double_entry_file_name = "REACHDataDoubleEntry_DATA_2024-04-08_1306.csv"
 
-# process redcap data and export into bids/phenotype
-proc_redcap(visit_data_path, data_de_path, overwrite = FALSE, return_data = FALSE)
+# assign paths to data downloaded from redcap
+visit_data_path = paste0(base_dir, "/bids/sourcedata/phenotype/", visit_file_name)
+data_de_path = paste0(base_dir, "/bids/sourcedata/phenotype/", double_entry_file_name)
+
+# process redcap data and export into bids/phenotype 
+redcap_data <- proc_redcap(visit_data_path, data_de_path, overwrite = FALSE, return_data = TRUE)
 
 #### process task (foodview, sst) data ####
-
-# assign path to base directory, which contains untouchedRaw/ and bids/
-base_wd = "/Users/baf44/projects/Keller_Marketing/ParticipantData/"
-
-# # move task data from untouchedRaw into bids/sourcedata
-# util_org_sourcedata(base_wd, overwrite = FALSE)
-# 
-# # process Food View task data and export into bids/rawdata
-# for (sub in subs) {
-#   # 
-# }
+task_data <- proc_task(base_wd = base_dir, overwrite = FALSE, return_data = TRUE)
