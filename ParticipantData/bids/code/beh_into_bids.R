7ee6f785b89a482709bbdeeed8d1ad79e0237fd1
bfuchs18 <bari.fuchs@gmail.com>
2024-05-02 16:06:01 -0400

    update visit_file_name; update proc_task() with additional args

--- ParticipantData/bids/code/beh_into_bids.R
+++ ParticipantData/bids/code/beh_into_bids.R
@@ -12,7 +12,7 @@ base_dir <- "/Users/baf44/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniv
 #### process redcap data ####
 
 # set redcap file names
-visit_file_name =  "FoodMarketingResilie_DATA_2024-04-08_1303.csv"
+visit_file_name =  "FoodMarketingResilie_DATA_2024-05-02_1044.csv"
 double_entry_file_name = "REACHDataDoubleEntry_DATA_2024-04-08_1306.csv"
 
 # assign paths to data downloaded from redcap
@@ -20,7 +20,22 @@ visit_data_path = paste0(base_dir, "/bids/sourcedata/phenotype/", visit_file_nam
 data_de_path = paste0(base_dir, "/bids/sourcedata/phenotype/", double_entry_file_name)
 
 # process redcap data and export into bids/phenotype 
-redcap_data <- proc_redcap(visit_data_path, data_de_path, overwrite = FALSE, return_data = TRUE)
+redcap_data <-
+  proc_redcap(visit_data_path,
+              data_de_path,
+              overwrite = TRUE,
+              return_data = TRUE)
 
-#### process task (foodview, sst) data ####
-task_data <- proc_task(base_wd = base_dir, overwrite = FALSE, return_data = TRUE)
+#### process task data ####
+# process data: food view, sst ... 
+
+task_data <-
+  proc_task(
+    base_wd = base_dir,
+    overwrite_parsed_rrv = FALSE,
+    overwrite_sourcedata = FALSE,
+    overwrite_rawdata_vector = c("all_tasks"),
+    #overwrite_rawdata_vector = c(),
+    overwrite_jsons = FALSE,
+    return_data = TRUE
+  )
