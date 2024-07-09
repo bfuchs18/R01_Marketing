26eea1ae5b6a2f39872f361f793564f4d52fe5d4
bfuchs18 <bari.fuchs@gmail.com>
2024-07-09 14:19:02 -0400

    update file name

--- ParticipantData/bids/code/beh_into_bids.R
+++ ParticipantData/bids/code/beh_into_bids.R
@@ -17,7 +17,7 @@ load_all("/Users/baf44/projects/dataREACHr")
 base_dir = "/Users/baf44/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniversity/b-childfoodlab_Shared/Active_Studies/MarketingResilienceRO1_8242020/ParticipantData/"
 
 # define names of redcap files to process
-visit_file_name =  "FoodMarketingResilie_DATA_2024-05-20_1520.csv"
+visit_file_name =  "FoodMarketingResilie_DATA_2024-06-26_1426.csv"
 double_entry_file_name = "REACHDataDoubleEntry_DATA_2024-04-08_1306.csv"
 
 # define Penn State user ID
@@ -46,8 +46,8 @@ qc_redcap(redcap_data)
 task_data <-
   proc_task(
     base_wd = base_dir,
-    overwrite_sourcedata = TRUE,
-    overwrite_rawdata = TRUE,
+    overwrite_sourcedata = FALSE,
+    overwrite_rawdata = FALSE,
     overwrite_jsons = TRUE,
     return_data = TRUE
   )
