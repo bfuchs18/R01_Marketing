9a62ada0f908100a3db401917b3b9c8fca67c568
Bari Fuchs <bari.fuchs@gmail.com>
2024-07-15 17:11:57 -0400

    init commit

---
+++ ParticipantData/bids/code/analyses/foodview/level_1/proc_functions/wrapper-python.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This script was created to run python data processing functions for a given subject. Running this script requires 2 command line arguements:
+(1) -s or --sub: integer with subject ID (e.g., -s 001)
+(2) -d or --bids-dir: string with path to bids directiry (e.g., -d "/path/to/bids/")
+
+By taking command line arguments, this script can be run via a SLURM script
+
+@author: baf44
+"""
+# for testing locally:
+# -d "/Users/bari/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniversity/b-childfoodlab_Shared/Active_Studies/MarketingResilienceRO1_8242020/ParticipantData/bids"
+
+#set up packages    
+import sys
+import os
+import argparse
+
+# import data processing functions
+import gen_uncensored_onsets
+import gen_censor_files
+import gen_regressor_file
+import gen_censored_onsets
+
+# process command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--sub", type=int, help="subject ID (e.g., 001)")
+parser.add_argument("-d", "--bidsdir", help="string with path to bids directiry (e.g., '/path/to/bids/'")
+args = parser.parse_args()
+
+if args.sub is None or args.bidsdir is None:
+    parser.print_help()
+    sys.exit(1)  # Exit with a non-zero status indicating an error
+else:
+    sub = str(args.sub).zfill(3)
+    bids_dir = args.bidsdir
+
+# define strings with paths for processing fucntions
+rawdata_dir = os.path.join(bids_dir, 'rawdata/')
+fmriprep_dir = os.path.join(bids_dir, 'derivatives', 'preprocessed', 'fmriprep_v2320')
+analysis_dir = os.path.join(bids_dir, 'derivatives', 'analyses', 'foodview')
+
+# call processing functions
+
+try:
+    gen_uncensored_onsets.gen_uncensored_onsets(sub = sub, rawdata_dir = rawdata_dir, analysis_dir = analysis_dir, overwrite=False)
+except:
+    print("Discontinuing gen_uncensored_onsets() for sub-" + sub)
+
+try:
+    gen_regressor_file.gen_regressor_file(sub = sub, fmriprep_dir = fmriprep_dir, analysis_dir = analysis_dir, overwrite = False)
+except:
+    print("Discontinuing gen_regressor_file() for sub-" + sub)
+
+
