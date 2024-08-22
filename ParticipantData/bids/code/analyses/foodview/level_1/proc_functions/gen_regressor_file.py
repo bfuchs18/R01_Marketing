f6df538c375b5fc6178c2e8a8a185cb8c3858ba9
bfuchs18 <bari.fuchs@gmail.com>
2024-08-22 11:21:08 -0400

    export data with average framewise displacement per run and overall

--- ParticipantData/bids/code/analyses/foodview/level_1/proc_functions/gen_regressor_file.py
+++ ParticipantData/bids/code/analyses/foodview/level_1/proc_functions/gen_regressor_file.py
@@ -19,14 +19,17 @@ from pathlib import Path
 
 # for debugging
 sub = 1
-fmriprep_dir = "/Users/baf44/OneDrive - The Pennsylvania State University/b-childfoodlab_Shared/Active_Studies/MarketingResilienceRO1_8242020/ParticipantData/bids/derivatives/preprocessed/fmriprep_v2320"
-analysis_dir = "/Users/baf44/OneDrive - The Pennsylvania State University/b-childfoodlab_Shared/Active_Studies/MarketingResilienceRO1_8242020/ParticipantData/bids/derivatives/analyses/foodview"
+fmriprep_dir = "/Users/baf44/projects/Keller_Marketing/ParticipantData/bids/derivatives/preprocessed/fmriprep_v2320"
+analysis_dir = "/Users/baf44/projects/Keller_Marketing/ParticipantData/bids/derivatives/analyses/foodview"
 overwrite = True
 
 def gen_regressor_file(sub, fmriprep_dir, analysis_dir, overwrite = False, return_regressordata_dict = False):
     """
-    This function will creates a CSV file with nuisance regressors for first-level analyses in AFNI based on fmriprep confound files for a given subject
-    The following variables will be included: trans_x, trans_y, trans_z, rot_x, rot_y, rot_z, csf, white_matter, trans_x_derivative1, trans_y_derivative1, trans_z_derivative1, rot_x_derivative1, rot_y_derivative1, rot_z_derivative1
+    This function exports 
+    (1) a CSV file with nuisance regressors for first-level analyses in AFNI based on fmriprep confound files for a given subject
+    (2) a CSV file with average framewise displacement, for inclusion as a group-level covariate
+
+    The following variables are nuisance regressors: trans_x, trans_y, trans_z, rot_x, rot_y, rot_z, csf, white_matter, trans_x_derivative1, trans_y_derivative1, trans_z_derivative1, rot_x_derivative1, rot_y_derivative1, rot_z_derivative1
 
     Inputs:
         sub 
@@ -84,15 +87,16 @@ def gen_regressor_file(sub, fmriprep_dir, analysis_dir, overwrite = False, retur
         print('No foodview confound files found for sub ' + str(sub))
         raise Exception()
 
-    ##################################
-    ### Create regressor dataframe ###
-    ##################################
+    #########################################
+    ### Extract data from fmriprep output ###
+    #########################################
 
     # Make list of nuisance regressors
     nui_regs =['trans_x', 'trans_y', 'trans_z', 'rot_x', 'rot_y', 'rot_z', 'csf', 'white_matter', 'trans_x_derivative1', 'trans_y_derivative1', 'trans_z_derivative1', 'rot_x_derivative1', 'rot_y_derivative1', 'rot_z_derivative1']
     
-    # create dictionary to store data in -- dataframes for each run will be appended to 'all-runs' list, and added in their own key-value pair
-    regressor_data_dict = {'all-runs': []}
+    # create dictionaries to store data in
+    regressor_data_dict = {'all-runs': []} # for each run, dataframe will be appended to 'all-runs' list, and added in their own key-value pair
+    fd_dict = {'all-runs': []} # for each run, list will be merged with 'all-runs' list, and added in their own key-value pair
 
     confound_files.sort()
     for file in confound_files: #loop through runs (each run has its own confoundfile)
@@ -116,15 +120,23 @@ def gen_regressor_file(sub, fmriprep_dir, analysis_dir, overwrite = False, retur
         # add run-specific regressor data to dictionary in new key-value pair
         regressor_data_dict[run] = run_regressors_data
 
-        # extend censordata_dict['all_runs'] with run_censordata
+        # extend regressor_data_dict['all_runs'] with regressor_data_dict
         regressor_data_dict['all-runs'].append(run_regressors_data)
+
+        # add framewise_displacement to fd_dict for run and 'all_runs'
+        fd_dict[run] = confound_data['framewise_displacement'].to_list()
+        fd_dict["all-runs"] = fd_dict["all-runs"] + fd_dict[run]
 
     # Concatenate the dataframes in 'all-runs' list into 1 dataframe
     regressor_data_dict['all-runs'] = pd.concat(regressor_data_dict['all-runs'], ignore_index=True)
 
-    ##############################
-    ### Export regressor files ###
-    ##############################
+    # Compute average fd for each key
+    avg_fd_dict = {key: [np.nanmean(value)] for key, value in fd_dict.items()}
+    avg_fd_df = pd.DataFrame.from_dict(avg_fd_dict)
+
+    ####################
+    ### Export files ###
+    ####################
 
     # define subject export dir
     sub_analysis_dir = os.path.join(analysis_dir, 'level_1/sub-' + str(sub) + '/')
@@ -132,6 +144,7 @@ def gen_regressor_file(sub, fmriprep_dir, analysis_dir, overwrite = False, retur
     # Make directory for export 
     Path(sub_analysis_dir).mkdir(parents=True, exist_ok=True)
     
+    # Export regressor file per run and overall
     for key in regressor_data_dict:
 
         # define output file name
@@ -149,6 +162,12 @@ def gen_regressor_file(sub, fmriprep_dir, analysis_dir, overwrite = False, retur
         else:
             print(key + ' regressor file already exist for sub ' + str(sub) + '. Use overwrite = True to overwrite')
 
+    # Export 1 file with average displacement values
+    fd_file_name = Path(os.path.join(sub_analysis_dir, 'sub-' + sub + '_ses-1_task-foodview_avg-fd.tsv'))
+
+    if not fd_file_name.exists() or overwrite:
+        print('Exporting average framewise displacement file for sub ' + str(sub))
+        avg_fd_df.to_csv(fd_file_name, index=False) 
 
     # return dictionary
     if return_regressordata_dict is True:
