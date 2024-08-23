6771ffd46a2c4ed42b2df3d76510abee2939474f
bfuchs18 <bari.fuchs@gmail.com>
2024-08-23 11:34:12 -0400

    replace gen_group_avgfd_data.py and gen_group_censor_summary.py with compile_lev1_tsv.py

---
+++ ParticipantData/bids/code/analyses/foodview/level_2/compile_lev1_tsv.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: baf44
+"""
+
+#set up packages    
+import os
+from pathlib import Path
+import pandas as pd
+
+# for debugging
+analysis_dir = "/Users/baf44/projects/Keller_Marketing/ParticipantData/bids/derivatives/analyses/foodview"
+tsv_identifier = "avg-fd"
+overwrite = True
+
+def compile_lev1_tsv(analysis_dir, tsv_identifier, overwrite = False):
+    """
+    This functions generates a compiled dataset of level_1 TSV files identified based on tsv_identifier. 
+
+    Inputs:
+        tsv_identifier (str) - input to rglob to identify TSV files that match 'level_1/sub*/*{tsv_identifier}*.tsv' to compile (e.g., "avg-fd", "censor-summary")
+        analysis_dir (str) - path to directory in bids/derivatives/analyses (e.g., "/Users/storage/group/klk37/default/R01_Marketing/bids/derivatives/analyses/foodview")
+        overwrite (boolean) - specify if output file should be overwritten (default = False)
+
+    Exports:
+        level_2/compiled_{tsv_identifier}.tsv: TSV with compiled data for all participants with level_1/sub*/*{tsv_identifier}*tsv
+    """
+
+    #######################
+    ### Check arguments ###
+    #######################
+
+    if not isinstance(analysis_dir, str):
+        raise TypeError("required argument analysis_dir must be a str")
+   
+    if not isinstance(tsv_identifier, str):
+        raise TypeError("required argument tsv_identifier must be a str")
+   
+    if not isinstance(overwrite, bool):
+        raise TypeError("argument overwrite must be boolean (True or False)")
+   
+    #######################
+    ### Compile data ###
+    #######################
+
+    # get list of level_1 files to compile
+    r_glob_str = 'level_1/sub*/*' + tsv_identifier + '*.tsv'
+    sub_files = list(Path(analysis_dir).rglob(r_glob_str))
+
+    # quit if no files sub_files
+    if not sub_files:
+        print('No files matching pattern ' + analysis_dir + r_glob_str + '. Check analysis_dir and tsv_identifier arguments')
+        raise Exception()
+
+    # initialize list to save subject dataframes to
+    dataframe_list = []
+
+    # create list of dataframes with subject data
+    for file_path in sub_files:
+
+        # extract file basename
+        file_name = file_path.name
+
+        #load subject's data as a dataframe
+        sub_data = pd.read_csv(str(file_path), sep = '\t', encoding = 'utf-8-sig', engine='python')
+
+        ## append dataframe to list
+        dataframe_list.append(sub_data)
+
+    # concatenate all dataframes into a single dataframes
+    group_data = pd.concat(dataframe_list, ignore_index=True)
+
+    # Make directory for export 
+    Path(os.path.join(analysis_dir, 'level_2')).mkdir(parents=True, exist_ok=True)
+
+    # define output file path
+    file_name = Path(os.path.join(analysis_dir, 'level_2', 'compiled_' + tsv_identifier + '.tsv'))
+
+    # export if doesnt exist or overwrite is True
+    if not file_name.exists() or overwrite:
+        print('Exporting level_2/' + str(file_name.name))
+        group_data.to_csv(str(file_name), sep = '\t', encoding='ascii', index = False, header=True)
+    else:
+        print('level_2/' + str(file_name.name) + ' exists. Use overwrite = True to overwrite')
--- ParticipantData/bids/code/analyses/foodview/level_2/wrapper-lev2-python.py
+++ ParticipantData/bids/code/analyses/foodview/level_2/wrapper-lev2-python.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-This script was created to run python data processing functions for a given subject. Running this script requires 2 command line arguements:
-(1) -s or --sub: integer with subject ID (e.g., -s 001)
-(2) -d or --bids-dir: string with path to bids directiry (e.g., -d "/path/to/bids/")
+This script was created to run group-level python data processing functions. Running this script requires 1 command line arguements:
+(1) -d or --bids-dir: string with path to bids directiry (e.g., -d "/path/to/bids/")
 
 By taking command line arguments, this script can be run via a SLURM script
 
 @author: baf44
 """
+# usage on roar collab:
+# >> conda activate pandas
+# >> python3 wrapper-lev2-python.py -d "/storage/group/klk37/default/R01_Marketing/bids/"
+
 # for testing locally:
-# -d "/Users/bari/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniversity/b-childfoodlab_Shared/Active_Studies/MarketingResilienceRO1_8242020/ParticipantData/bids"
+# >> python3 wrapper-lev2-python.py -d "/Users/bari/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniversity/b-childfoodlab_Shared/Active_Studies/MarketingResilienceRO1_8242020/ParticipantData/bids"
 
 #set up packages    
 import sys
@@ -18,10 +21,10 @@ import os
 import argparse
 
 # import data processing functions
-import gen_group_avgfd_data
+import compile_lev1_tsv
 import gen_covariate_table
-import gen_group_censor_summary
 import gen_id_list
+
 
 
 # process command line arguments
@@ -45,14 +48,15 @@ print("\n********************************************")
 print(f"**** Running group-level python functions ****")
 print("***********************************************")
 
-print("\n*** Running function to compile avg framewise displacement data across subs ***")
-gen_group_avgfd_data.gen_group_avg_data(analysis_dir = analysis_dir, overwrite=False)
 
-print("\n*** Running function to XXXX ***")
-gen_covariate_table.gen_uncensored_onsets(analysis_dir = analysis_dir, overwrite=False)
+print("\n*** Running function to compile level_1 avg-fd data across subs ***")
+compile_lev1_tsv.compile_lev1_tsv(analysis_dir = analysis_dir, tsv_identifier='avg-fd', overwrite=True)
 
-print("\n*** Running function to XXXX ***")
-gen_group_censor_summary.gen_uncensored_onsets(analysis_dir = analysis_dir, overwrite=False)
+print("\n*** Running function to compile level_1 censor-summary data across subs ***")
+compile_lev1_tsv.compile_lev1_tsv(analysis_dir = analysis_dir, tsv_identifier='censor-summary', overwrite=True)
+
+print("\n*** Running function to generate covariate table ***")
+gen_covariate_table.gen_covariate_table(analysis_dir = analysis_dir, overwrite=True)
 
 print("\n*** Running function to XXXX ***")
 gen_id_list.gen_uncensored_onsets(analysis_dir = analysis_dir, overwrite=False)
