7accacc758cffad0ef3997f1891cd8bca7e6013b
bfuchs18 <bari.fuchs@gmail.com>
2025-01-03 15:43:00 -0500

    update

--- ParticipantData/bids/code/config_redcap_sourcedata.R
+++ ParticipantData/bids/code/config_redcap_sourcedata.R
@@ -3,5 +3,5 @@
 # File names should be updated in this config file only, which can be sourced with: 
 ## source("ParticipantData/bids/code/config_redcap_sourcedata.R")
 
-visit_file_name =  "FoodMarketingResilie_DATA_2024-12-08_1402.csv"
-double_entry_file_name = "REACHDataDoubleEntry_DATA_2024-12-17_1040.csv"
+visit_file_name =  "FoodMarketingResilie_DATA_2025-01-03_1321.csv"
+double_entry_file_name = "REACHDataDoubleEntry_DATA_2025-01-03_1320.csv"
